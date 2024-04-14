eace49d8166bd4b51b31bf5becc1442fc5c04678
ashtishad <ashefgo@gmail.com>
2024-04-15 02:39:48 +0600

    Leetcode 121. Best Time to Buy and Sell Stock.
    Added more testcases.

--- array/121_best_time_to_buy_and_sell_stock/max_profit_test.go
+++ array/121_best_time_to_buy_and_sell_stock/max_profit_test.go
@@ -1,22 +1,31 @@
 package array
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test_maxProfit(t *testing.T) {
+func Test_MaxProfit(t *testing.T) {
 	tests := []struct {
 		name   string
 		prices []int
 		want   int
 	}{
 		{"LC-1", []int{7, 1, 5, 3, 6, 4}, 5},
-		{"Increasing Prices", []int{7, 6, 4, 3, 1}, 0},
-		{"Single Input", []int{1}, 0},
+		{"LC-2", []int{7, 6, 4, 3, 1}, 0},
+		{"Single Element", []int{5}, 0},
+		{"Increasing prices", []int{1, 2, 3, 4, 5}, 4},
+		{"No profit, decreasing prices", []int{8, 7, 6, 5, 4, 3, 2, 1}, 0},
+		{"No profit, constant prices", []int{2, 2, 2, 2, 2}, 0},
+		{"Buy on day 2, sell on day 6", []int{5, 1, 8, 6, 2, 10}, 9},
+		{"Buy on day 4, sell on day 7", []int{3, 3, 5, 0, 0, 3, 1, 4}, 4},
+		{"Buy on day 0, sell on day 7", []int{1, 2, 3, 4, 5, 6, 7, 8}, 7},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := maxProfitBF(tt.prices); got != tt.want {
-				t.Errorf("maxProfit() = %v, want %v", got, tt.want)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := maxProfit(tc.prices)
+			if got != tc.want {
+				t.Errorf("got:%d, want:%d", got, tc.want)
 			}
 		})
 	}
