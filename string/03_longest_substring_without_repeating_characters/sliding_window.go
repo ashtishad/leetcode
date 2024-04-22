3db18e0db23dccb4a90af4e7295b69f09045fcfd
ashtishad <ashefgo@gmail.com>
2024-04-22 11:39:31 +0600

    3. Longest Substring Without Repeating Characters

--- string/03_longest_substring_without_repeating_characters/sliding_window.go
+++ string/03_longest_substring_without_repeating_characters/sliding_window.go
@@ -1,17 +1,15 @@
 package main
 
 func lengthOfLongestSubstring(s string) int {
-	seen := make(map[rune]int)
+	seen := make(map[rune]int) // k:ch v:idx
 	var l, res int
 
 	for r, ch := range s {
-		seen[ch]++
-
-		for seen[ch] > 1 {
-			seen[rune(s[l])]--
-			l++
+		if idx, ok := seen[ch]; ok && idx >= l {
+			l = idx + 1 // start new window
 		}
 
+		seen[ch] = r
 		res = max(res, r-l+1)
 	}
 
